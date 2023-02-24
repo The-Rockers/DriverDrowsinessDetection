ba085bfdce538f62d8892133d7098fcfe0215436
Rohit Nair <6954311+rnair98@users.noreply.github.com>
2023-02-24 14:13:31 -0500

    expo + tensorflow.js fix with node@16

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, TextInput, View, Button, Linking} from 'react-native';
+import * as tf from '@tensorflow/tfjs'
+import * as FileSystem from 'expo-file-system';
+import {bundleResourceIO, decodeJpeg} from '@tensorflow/tfjs-react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text> This is a test element. Holy cow this is awesome ACTUALLY!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <TextInput style={styles.textStyling}
+                    placeholder="User Name"
+                />
+                <TextInput style={styles.textStyling}
+                    placeholder="Password"
+                />
+            </View>
+            <View style={styles.userAccount}>
+                <Button title="Log In" />
+                <Button title="Log Out" />
+            </View>
+            <View style={styles.utilContainer}>
+                <Button title="Activate Drowisness Detection" />
+                <Button onPress={() => console.log("press")} title="Deactivate Drowsiness Detection" />
+                <Button onPress={() => getCameraPermissionStatus} title="Configure Settings" />
+                <Text> This works fine for now</Text>
+            </View>
+            <View>
+                {
+
+                }
+            </View>
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+    textStyling: {
+        borderWidth: 1,
+        borderColor: "blue",
+        padding: 5,
+        marginBottom: 5,
+    },
+
+    buttonStyling: {
+        backgroundColor: 'black',
+        color: "red",
+    },
+
+    container: {
+        paddingTop: 35,
+        paddingBottom: 35,
+
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    userAccount: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBotton: 50,
+    },
+
+    utilContainer: {
+        flex: 4,
+    }
 });
