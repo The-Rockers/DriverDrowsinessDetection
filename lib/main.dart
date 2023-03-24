8d27b4b58910552c8631e55c412538706ab5b9cc
Aman Koua <akoua@umich.edu>
2023-03-24 00:24:49 -0400

    successfully enabled bluetooth scanning on android device

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+// https://www.ubiqueiot.com/posts/flutter-reactive-ble
+// Tutorial Reference
+
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:location_permissions/location_permissions.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,24 +21,60 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp>{
 
-  //final flutterReactiveBle = FlutterReactiveBle();
-  String text = "test";
+  // Some state management stuff
+  bool _foundDeviceWaitingToConnect = false;
+  bool _scanStarted = false;
+  bool _connected = false;
+// Bluetooth related variables
+  late DiscoveredDevice _ubiqueDevice;
+  final flutterReactiveBle = FlutterReactiveBle();
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late QualifiedCharacteristic _rxCharacteristic;
+// These are the UUIDs of your device
+  final Uuid serviceUuid = Uuid.parse("75C276C3-8F97-20BC-A143-B354244886D4");
+  final Uuid characteristicUuid = Uuid.parse("6ACF4F08-CC9D-D495-6B41-AA7E60C4E8A6");
+  
+  String text = " ";
+
+  void _startScan() async {
+// Platform permissions handling stuff
+    bool permGranted = false;
+    setState(() {
+      _scanStarted = true;
+    });
+    PermissionStatus permission;
+    if (Platform.isAndroid) {
+      
+      // For some reason, bluetooth scanning won't work unless location
+      // is on and paermissions are granted. No idea why.
+
+      permission = await LocationPermissions().requestPermissions();
+      if (permission == PermissionStatus.granted) permGranted = true;
+    } else if (Platform.isIOS) {
+      permGranted = true;
+    }
+// Main scanning logic happens here ⤵️
+    if (permGranted) {
+      _scanStream = flutterReactiveBle
+          .scanForDevices(withServices: []).listen((device) {
+
+            print("------------ Scanning for devices ------------------");
+            print("Device: " + device.name);
+
+            if(device.name != ""){
+              setState(){
+                text += device.name;
+              }
+            }
+      });
+    }
+  }
 
   void _handleTap() {
     setState(() {
       text += "a";
     });
   }
-
-  /*
-  void scan(){
-    flutterReactiveBle.scanForDevices(withServices: [], /*scanMode: ScanMode.lowLatency*/).listen((device) {
-      print("Scanning for devices");
-    }, onError: () {
-      print("Error in scanning for devices");
-    });
-  }
-  */
 
   @override
   Widget build(BuildContext context) {
@@ -46,8 +89,8 @@ class MyAppState extends State<MyApp>{
               const SizedBox(height: 30),
               ElevatedButton(
                 style: null,
-                onPressed: _handleTap,
-                child: Text("Tap me"),
+                onPressed: _startScan,
+                child: Text("Start Scan"),
               ),
               Text(text),
             ],
